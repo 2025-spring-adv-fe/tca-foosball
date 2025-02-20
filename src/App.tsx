6513dbcf9357489bf4312c63e72ea0dea0073083
papadawda <dknjai1@madisoncollege.edu>
2025-02-20 12:09:42 -0600

    Learned About padding and margins
    Also saw some font, color Tailwind and DasiyUI styling..

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,30 @@ function App() {
   
 
   return (
-   <div>
-    <h1
-      className='text-2xl font-bold'
-    >
-      TCA Foos Ball
-    </h1>
-    <button
-      className='btn btn-secondary btn-soft btn-xl'
-      
-    >
-      Play Foos Ball
-    </button>
+    <div>
+      <h1
+        className='text-2xl font-bold bg-base-300 p-4 text-secondary'
+      >
+        TCA Foos Ball
+      </h1>
+      <div 
+        className="p-4"
+        
+        >
+          <button
+        className='btn btn-secondary btn-soft btn-xl'
+        
+      >
+        Play Foos Ball
+      </button>
+      <h2 
+        className='mt-3 text-xl front-semi-bold'
 
-   </div>
+      >
+        Leaderbored..
+      </h2>
+      </div>
+    </div>
   )
 }
 
