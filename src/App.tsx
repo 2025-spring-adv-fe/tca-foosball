ca270a78db7c72af52ec04db6c3a0e665750308d
papadawda <dknjai1@madisoncollege.edu>
2025-02-21 18:27:32 -0600

    Learned my card ui-hammer..
    And played around with "infinite" scrolling
    
    Looks, good :-)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <div>
@@ -11,22 +11,112 @@ function App() {
       >
         TCA Foos Ball
       </h1>
-      <div 
+      <div
         className="p-4"
-        
-        >
-          <button
-        className='btn btn-secondary btn-soft btn-xl'
-        
-      >
-        Play Foos Ball
-      </button>
-      <h2 
-        className='mt-3 text-xl front-semi-bold'
 
       >
-        Leaderbored..
-      </h2>
+        <button
+          className='btn btn-secondary btn-soft btn-xl'
+
+        >
+          Play Foos Ball
+        </button>
+        <h2
+          className='mt-3 text-xl front-semi-bold'
+
+        >
+          Leaderbored..
+        </h2>
+
+
+
+
+
+
+        <div 
+        className="card w-full bg-base-100 card-lg shadow-sm mt-4"
+        
+        >
+          <div 
+            className="card-body"
+          >
+            <h2 
+              className="card-title"
+              
+            >
+              Leaderboard...
+              </h2>
+            <p>
+              Leaderboard coming soon ....
+            </p>
+          </div>
+        </div>
+
+
+        <div 
+        className="card w-full bg-base-100 card-lg shadow-sm mt-4"
+        
+        >
+          <div 
+            className="card-body"
+          >
+            <h2 
+              className="card-title"
+              
+            >
+              Leaderboard...
+              </h2>
+            <p>
+              Leaderboard coming soon ....
+            </p>
+          </div>
+        </div>
+
+
+        <div 
+        className="card w-full bg-base-100 card-lg shadow-sm mt-4"
+        
+        >
+          <div 
+            className="card-body"
+          >
+            <h2 
+              className="card-title"
+              
+            >
+              Leaderboard...
+              </h2>
+            <p>
+              Leaderboard coming soon ....
+            </p>
+          </div>
+        </div>
+
+
+
+        <div 
+        className="card w-full bg-base-100 card-lg shadow-sm mt-4"
+        
+        >
+          <div 
+            className="card-body"
+          >
+            <h2 
+              className="card-title"
+              
+            >
+              Leaderboard...
+              </h2>
+            <p>
+              Leaderboard coming soon ....
+            </p>
+          </div>
+        </div>
+
+
+
+
+
       </div>
     </div>
   )
