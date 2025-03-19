2655af051ed10c5542a44109156543e358a9ba15
papadawda <dknjai1@madisoncollege.edu>
2025-03-19 14:57:54 -0500

    Moved Home to it file
    I need to copy that file 2 times

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,7 @@
    
   } from 'react-router';
 
-  const Home = () => {
-    
-    return (
-      <>
-          <h3
-          className='text-2xl font-bold'
-        >
-          Home
-        </h3>
-          <button className="btn btn-active btn-secondry btn-lg mt-4"
-        >
-          Play Foosball
-        </button>
-      </>
-   
-    );
-  };
+  import { Home } from './Home'
   
   const  App = () => {
     
