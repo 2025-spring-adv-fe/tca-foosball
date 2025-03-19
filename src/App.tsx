02df2ea8cc2c21c9d151b427e189d0668acff2b0
papadawda <dknjai1@madisoncollege.edu>
2025-03-19 13:42:10 -0500

    React basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
 import { useState } from 'react'
 import './App.css'
 
+<<<<<<< Updated upstream
 function App() {
   
+=======
+  import './App.css'
+
+  interface AppProps {
+    timestamp: string;
+    magicNumber: number;
+  }
+
+  const App: React.FC<AppProps> = (
+    // props: any
+
+    // Destructuring....
+  {
+    timestamp
+    , magicNumber
+  }
+) => {
+    
+
+    console.log(
+      "App component Func Called !!!"
+    // , typeof(props)
+    // , props
+  );
+>>>>>>> Stashed changes
 
   return (
    <div>
