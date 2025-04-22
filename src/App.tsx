d99ec6061059d86de42b7cb81b109903905a8ec3
papadawda <dknjai1@madisoncollege.edu>
2025-04-21 23:52:45 -0500

    Button is working
    But there is still a bug

--- src/App.tsx
+++ src/App.tsx
@@ -50,6 +50,8 @@
 
   const [currentPlayers, setCurrentPlayers] = useState<string[]>([]);
 
+  const [darkMode, setDarkMode] = useState(false);
+
 // 
 // Others code (not hooks)
 //
@@ -62,7 +64,8 @@ const addNewGameResult = (newGameResult: GameResult) => setGameResults(
 
   return (
    <div
-    className='p-0'
+    className='p-0 overflow-x-hidden min-h-screen'
+    data-theme={darkMode ? "dark" : "light"}
    >
     <div className="navbar bg-base-300 shadow-lg overflow-x-hidden flex"
     >
@@ -73,7 +76,12 @@ const addNewGameResult = (newGameResult: GameResult) => setGameResults(
 
       <label className="swap swap-rotate ml-auto">
         {/* this hidden checkbox controls the state */}
-        <input type="checkbox" />
+        <input 
+          type="checkbox"
+          onClick={
+            () => setDarkMode(!darkMode)
+          }
+        />
 
         {/* sun icon */}
         <svg
@@ -93,10 +101,6 @@ const addNewGameResult = (newGameResult: GameResult) => setGameResults(
             d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
         </svg>
       </label>
-
-
-
-
     </div>
     <div className="p-4"
     >
